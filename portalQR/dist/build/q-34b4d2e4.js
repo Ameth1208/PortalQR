05bb8882988ec587f674bded178ce914ee37f589
Ameth <Amethgm@gmail.com>
2023-08-25 16:02:23 -0500

    perf: :wastebasket: se borraron archivos
    se coloca en el gitnore portalQR

--- portalQR/dist/build/q-34b4d2e4.js
+++
@@ -1,1 +0,0 @@
-import{h as f}from"./q-80a093d9.js";export{f as default};
--- portalQR/dist/build/q-8968fc5f.js
+++
@@ -1,1 +0,0 @@
-import{_ as r}from"./q-80a093d9.js";const _=()=>r(()=>import("./q-45aab3b6.js"),[]);export{_ as Serviceworker};
